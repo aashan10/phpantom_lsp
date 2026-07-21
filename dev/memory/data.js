bf3343926719b7183d064bad1e1854365d10c96b
github-action-benchmark <github@users.noreply.github.com>
2026-07-21 18:15:52 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 10ccb3942bf66540b36502589cf832f43a680712

---
+++ dev/memory/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784657752171,
+  "repoUrl": "https://github.com/aashan10/phpantom_lsp",
+  "entries": {
+    "PHPantom Memory Usage": [
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "10ccb3942bf66540b36502589cf832f43a680712",
+          "message": "Add support for more Blade directives",
+          "timestamp": "2026-07-21T17:13:25+02:00",
+          "tree_id": "0df520416c21fe2310698b5170ea882591d9e6b9",
+          "url": "https://github.com/aashan10/phpantom_lsp/commit/10ccb3942bf66540b36502589cf832f43a680712"
+        },
+        "date": 1784657751435,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 54.1,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 111.9,
+            "unit": "MiB"
+          }
+        ]
+      }
+    ]
+  }
+}
